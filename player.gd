2868b37261de4cd9da94690db47570fa710271fb
cookie1170 <maksimhovanets@gmail.com>
2024-11-03 16:25:31 +1000

    coyote time go brrrrrrrrrrrrrrrrrrrrrrr

--- player.gd
+++ player.gd
@@ -19,9 +19,20 @@ class_name Player
 @onready var accel : float = speed / accel_time_sec
 @onready var decel : float = speed / decel_time_sec
 
+## nodes
+@onready var coyote_timer : Timer = %CoyoteTimer
+
+## other variables
+var can_jump : bool
+
+
 # processes physics (duh)
 func _physics_process(delta: float) -> void:
 
+	# sets the can_jump variable to true if you're on floor
+	if is_on_floor():
+		can_jump = true
+	
 	# sets gravity variable to equal jump gravity if you're going up
 	# and fall gravity if you're going down
 	var gravity = jump_grav if velocity.y < 0 else fall_grav
@@ -29,6 +40,11 @@ func _physics_process(delta: float) -> void:
 	# apply gravity
 	if not is_on_floor() and velocity.y <= terminal_velocity:
 		velocity.y += gravity * delta
+	
+	# start the coyote timer if you're not on floor and you can jump
+	if not is_on_floor() and can_jump and coyote_timer.is_stopped():
+		coyote_timer.start()
+
 	# makes it so you can't go above terminal velocity
 	if velocity.y > terminal_velocity:
 		velocity.y = terminal_velocity
@@ -43,7 +59,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, decel * delta)
 
 	# makes you jump if you press the jump button
-	if Input.is_action_just_pressed('jump') and is_on_floor():
+	if Input.is_action_just_pressed('jump') and can_jump:
 		jump()
 
 	move_and_slide() # move and slide makes the player move
@@ -52,3 +68,9 @@ func _physics_process(delta: float) -> void:
 # sets your velocity to the jump velocity if called
 func jump():
 	velocity.y = jump_vel
+	can_jump = false
+
+
+# set can_jump to false if you run out of coyote time
+func _on_coyote_timeout() -> void:
+	can_jump = false
